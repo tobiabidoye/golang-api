f999ea5ee89f143debb5d599bf6c4dc886dd6d94
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-01-23 23:35:44 -0500

    added routes and tested them

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+    "task-manager/routes"
+    "task-manager/database"
+)
+
+
+func main(){
+    database.Connect()
+    router := routes.SetupRouter()
+    
+    router.Run(":8080")
+
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -5,7 +5,7 @@ import(
     "github.com/gin-gonic/gin"
 )
 
-func setupRouter() *gin.Engine{
+func SetupRouter() *gin.Engine{
     router := gin.Default()
     //routes for tasks
     router.GET("/tasks", func(c * gin.Context){
